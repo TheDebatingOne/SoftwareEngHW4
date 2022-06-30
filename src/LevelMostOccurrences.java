b20f655a202549e954bc084a68a730de945643b5
Guy Caspi <guy.caspi@campus.technion.ac.il>
2022-06-30 17:20:21 +0300

    recursion

--- src/LevelMostOccurrences.java
+++ src/LevelMostOccurrences.java
@@ -1,6 +1,20 @@
+import java.util.ArrayDeque;
 public class LevelMostOccurrences {
     public static int getLevelWithMostOccurrences(BinNode<Integer> node, int num) {
         // TODO: Add your code here...
+        int treeHeight = treeHeight(node);
+
+
         return 0;
+    }
+
+
+    public static <E> int treeHeight(BinNode<E> node) {
+        if (node == null) return 0;
+        int leftHeight = 0, rightHeight = 0;
+        if (node.getLeft() != null) leftHeight = treeHeight(node.getLeft());
+        if (node.getRight() != null) rightHeight = treeHeight(node.getRight());
+        int max = (leftHeight > rightHeight ? leftHeight : rightHeight);
+        return max + 1; //adding 1 for current level...
     }
 }
