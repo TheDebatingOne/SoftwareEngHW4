629fbc9b52794e40662c35e5ba18786794cf92a1
Guy Caspi <guy.caspi@campus.technion.ac.il>
2022-06-30 18:10:43 +0300

    mylock

--- src/MyReentrantLock.java
+++ src/MyReentrantLock.java
@@ -1,7 +1,9 @@
 public class MyReentrantLock implements Lock {
+    private boolean locked;
     @Override
     public void acquire() {
-
+        while (!this.locked); //wait
+        this.locked = true;
     }
 
     @Override
