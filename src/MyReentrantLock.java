a3149d1e77ed2e1385ac5d0c45342153124994ba
Guy Caspi <guy.caspi@campus.technion.ac.il>
2022-06-30 19:22:23 +0300

    a

--- src/MyReentrantLock.java
+++ src/MyReentrantLock.java
@@ -1,12 +1,13 @@
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class MyReentrantLock implements Lock {
-    private AtomicBoolean locked;
-    private Thread locker;
+    private AtomicBoolean locked; //the status of the lock
+    private Thread owner; //the owner of the lock (aka the thread that locked him)
     @Override
     public void acquire() {
         while (!this.locked.get()); //wait until not locked
-        this.locked.set(true);
+        this.locked.set(true); //locking
+        this.owner = Thread.currentThread(); //setting the new owner
     }
 
     @Override
@@ -15,11 +16,15 @@ public class MyReentrantLock implements Lock {
 
     @Override
     public void release() {
-        if (!this.locked.get() || )
+        if (!this.locked.get() || !Thread.currentThread().equals(owner)) throw new IllegalReleaseAttempt();
+        //TODO: maybe compare thread IDs instead of equals?
+        this.locked.set(false); //releasing
+        this.owner = null;
     }
 
     @Override
     public void close() throws Exception {
 
     }
+
 }
