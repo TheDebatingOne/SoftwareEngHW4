2c9df1b028e175ad00156047f36bdd60b02dac15
TheDebatingOne <aloni.eitan@gmail.com>
2022-06-30 18:44:30 +0300

    Added Javadocs to LevelMostOccurrences

--- src/LevelMostOccurrences.java
+++ src/LevelMostOccurrences.java
@@ -1,7 +1,11 @@
-import java.util.ArrayDeque;
 public class LevelMostOccurrences {
+    /**
+     * Finds the level of a binary tree with the most occurrences of a specific value
+     * @param node The root of the tree
+     * @param num The value to count occurrences of
+     * @return The level with the most occurrences of num
+     */
     public static int getLevelWithMostOccurrences(BinNode<Integer> node, int num) {
-        // TODO: Add your code here...
         int treeHeight = treeHeight(node);
         int occurrencesPerLevel;
         int maxOccurrences = 0;
@@ -16,27 +20,30 @@ public class LevelMostOccurrences {
         return maxLevel;
     }
 
-    public static void createLevelQueue(BinNode<Integer> node, int remainingLevels, ArrayDeque<Integer> levelQueue){
-        if(remainingLevels == 0) levelQueue.add(node.getData()); //if no remaining levels
-        if (node.getRight() != null) createLevelQueue(node.getRight(), remainingLevels-1, levelQueue);
-        if (node.getLeft() != null) createLevelQueue(node.getLeft(), remainingLevels -1, levelQueue);
-    }
-    //TODO: is it necessary???
-
+    /**
+     * Calculates the number of occurrences of a value in a specific level of a binary tree
+     * @param node The root of the tree
+     * @param num The value to count occurrences of
+     * @param remainingLevels The level of the tree to check
+     * @return The number of times num appears in the specified level of the tree
+     */
     public static int levelOccurrence(BinNode<Integer> node, int num, int remainingLevels){
         int occurrences = 0;
-        if (remainingLevels == 0){ //if no more remaining levels
-            if (node.getData() == num) return 1;
-            else return 0;
-        }
-        if (node.getRight() != null) occurrences += levelOccurrence(node.getRight(), num, remainingLevels -1);
-        if (node.getLeft() != null) occurrences += levelOccurrence(node.getLeft(), num, remainingLevels - 1);
+        if (remainingLevels == 0) //if no more remaining levels
+            return node.getData() == num? 1: 0;
+        if (node.getRight() != null)
+            occurrences += levelOccurrence(node.getRight(), num, remainingLevels -1);
+        if (node.getLeft() != null)
+            occurrences += levelOccurrence(node.getLeft(), num, remainingLevels - 1);
         return occurrences;
     }
 
-
-
-
+    /**
+     * Finds the height of a binary tree
+     * @param node The root of the tree
+     * @return The height of the tree
+     * @param <E> The type of data in the nodes
+     */
     public static <E> int treeHeight(BinNode<E> node) {
         if (node == null) return 0;
         int leftHeight = 0, rightHeight = 0;
